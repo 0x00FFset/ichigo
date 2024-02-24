c66d52912416967a004cfb99ed2ed21374865b08
pierrot <pierrot@offset.cx>
2024-02-24 21:09:06 +0000

    Add version checking functionality and update NavBar component

--- app.go
+++ app.go
@@ -2,12 +2,35 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"net/http"
+	"os"
 )
 
 // App struct
 type App struct {
 	ctx context.Context
+}
+
+type AppMetadata struct {
+	Info struct {
+		ProductVersion string `json:"productVersion"`
+	} `json:"info"`
+}
+
+type GitHubRelease struct {
+    TagName string `json:"tag_name"`
+    Name    string `json:"name"`
+    HtmlUrl string `json:"html_url"`
+}
+
+type CustomResponseVersion struct {
+    Version string `json:"version"`
+    Url     string `json:"url"`
+    Message string `json:"message"`
+	Color   string `json:"color"`
+	DisableButton string `json:"disableButton"`
 }
 
 // NewApp creates a new App application struct
@@ -24,4 +47,81 @@ func (a *App) startup(ctx context.Context) {
 // Greet returns a greeting for the given name
 func (a *App) Greet(name string) string {
 	return fmt.Sprintf("Hello %s, It's show time!", name)
+}
+
+// check for updates
+func (a *App) CheckForUpdates() string {
+    // The GitHub repository you're interested in (change "owner" and "repo" to the actual values)
+    owner := "0x00FFset"
+    repo := "ichigo"
+	upToDate := "You're currently using the latest version of Ichigo."
+	needUpdate := "There's a new version of Ichigo available. Would you like to download it?"
+	successColor := "text-green-400"
+	errorColor := "text-red-400"
+	disableButton := "text-white opacity-20 cursor-not-allowed"
+	
+	release, err := getLatestRelease(owner, repo)
+    if err != nil {
+        fmt.Println("Error:", err)
+        os.Exit(1)
+    }
+
+    response := CustomResponseVersion{
+        Version: release.TagName,
+        Url:     release.HtmlUrl,
+    }
+	installedVersion := CheckVersion();
+
+	if release.TagName == installedVersion {
+		response.Message = upToDate
+		response.Color = successColor
+		response.DisableButton = disableButton
+	} else {
+		response.Message = needUpdate
+		response.Color = errorColor
+		response.DisableButton = ""
+	}
+
+		jsonResponse, err := json.MarshalIndent(response, "", "  ")
+		if err != nil {
+			fmt.Println("Error marshaling JSON:", err)
+			os.Exit(1)
+		}
+
+		return fmt.Sprint(string(jsonResponse))
+}
+
+func getLatestRelease(owner, repo string) (*GitHubRelease, error) {
+    url := fmt.Sprintf("https://api.github.com/repos/%s/%s/releases/latest", owner, repo)
+
+    resp, err := http.Get(url)
+    if err != nil {
+        return nil, err
+    }
+    defer resp.Body.Close()
+
+    var release GitHubRelease
+    if err := json.NewDecoder(resp.Body).Decode(&release); err != nil {
+        return nil, err
+    }
+
+    return &release, nil
+}
+
+func CheckVersion() string {
+    // Assuming `wails.json` is in the current directory
+    content, err := os.ReadFile("wails.json")
+    if err != nil {
+        fmt.Printf("Error reading wails.json: %v\n", err)
+        return ""
+    }
+
+    var metadata AppMetadata
+    err = json.Unmarshal(content, &metadata)
+    if err != nil {
+        fmt.Printf("Error parsing JSON: %v\n", err)
+        return ""
+    }
+
+    return metadata.Info.ProductVersion
 }
--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,3 +1,35 @@
+
+<script setup>
+import { reactive, onMounted } from 'vue'
+import { CheckForUpdates } from '../../wailsjs/go/main/App'
+
+
+const data = reactive({
+    url: "",
+    checkMessage: "Checking for updates...",
+    version: "0.0",
+    color: "text-cyan-200",
+    disableButton: ""
+})
+
+async function checkForUpdates() {
+    try {
+        const result = await CheckForUpdates(); // Assuming CheckForUpdates is a function that returns a Promise
+        const parsed = JSON.parse(result);
+        data.disableButton = parsed.disableButton;
+        data.color = parsed.color;
+        data.checkMessage = parsed.message;
+        data.version = parsed.version;
+        data.url = parsed.url;
+    } catch (error) {
+        console.error('Error checking for updates:', error);
+        // Handle the error appropriately
+    }
+}
+
+
+</script>
+
 <template>
     <div class="navbar bg-base-100">
         <div class="flex-1">
@@ -9,17 +41,21 @@
                             d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293z" />
                         <path d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293z" />
                     </svg></a>
-                <a class="btn btn-ghost">Currently Watching</a>
-                <a class="btn btn-ghost">Completed</a>
-                <a class="btn btn-ghost">On Hold</a>
-                <a class="btn btn-ghost">Dropped</a>
-                <a class="btn btn-ghost">Plan to watch</a>
-
+                <a class="btn btn-ghost">Catalog</a>
+                <a class="btn btn-ghost">Installed</a>
+                <a class="btn btn-ghost">Uninstall</a>
             </div>
         </div>
         <div class="flex-none gap-2">
             <div class="form-control">
-                <input type="text" placeholder="Search for an anime" class="input input-bordered w-24 md:w-auto" />
+                <a onclick="versionModal.showModal()" @click="checkForUpdates" class="btn btn-ghost"><svg
+                        xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2z" />
+                        <path
+                            d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466" />
+                    </svg>
+                    Check for updates</a>
             </div>
             <div class="dropdown dropdown-end">
                 <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
@@ -30,4 +66,18 @@
             </div>
         </div>
     </div>
+
+    <dialog id="versionModal" class="modal">
+        <div class="modal-box">
+            <h3 class="font-bold text-lg">Ichigo Update</h3>
+            <p :class="data.color" class="py-4">{{ data.checkMessage }}</p>
+            <a :href="[data.url]" target="_blank" class="py-4 text-cyan-200 underline">Release: {{ data.version }}</a>
+            <div class="modal-action">
+                <form method="dialog">
+                    <button class="btn mr-2">Close</button>
+                </form>
+                <button :class="['btn btn-primary', data.disableButton]">Update</button>
+            </div>
+        </div>
+    </dialog>
 </template>
